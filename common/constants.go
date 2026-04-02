6153cb1ec98d2f0ffdb45354f21d4aced6384adc
renmengwen <mwren1220@163.com>
2026-04-03 03:06:35 +0800

    docs(common): 更新系统名称常量值为"神州 AI API 服务平台"
    更新系统名称常量以反映最新的产品品牌名称

--- common/constants.go
+++ common/constants.go
@@ -12,7 +12,7 @@ import (
 
 var StartTime = time.Now().Unix() // unit: second
 var Version = "v0.0.0"            // this hard coding will be replaced automatically when building, no need to manually change
-var SystemName = "New API"
+var SystemName = "神州 AI API 服务平台"
 var Footer = ""
 var Logo = ""
 var TopUpLink = ""
