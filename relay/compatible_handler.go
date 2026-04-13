ff6a172c3bc4c5f424d442f73d32cf2b009dd542
renmengwen <mwren1220@163.com>
2026-04-13 09:08:51 +0800

    fix(playground): route volcengine responses models via relay

--- relay/compatible_handler.go
+++ relay/compatible_handler.go
@@ -75,7 +75,7 @@ func TextHelper(c *gin.Context, info *relaycommon.RelayInfo) (newAPIError *types
 	if info.RelayMode == relayconstant.RelayModeChatCompletions &&
 		!passThroughGlobal &&
 		!info.ChannelSetting.PassThroughBodyEnabled &&
-		service.ShouldChatCompletionsUseResponsesGlobal(info.ChannelId, info.ChannelType, info.OriginModelName) {
+		shouldUseResponsesForChatCompletions(info, c.GetString("model_mapping")) {
 		applySystemPromptIfNeeded(c, info, request)
 		usage, newApiErr := chatCompletionsViaResponses(c, info, adaptor, request)
 		if newApiErr != nil {
