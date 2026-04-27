9a312718cb5c2ce1c41830e0e40d5087dac6d265
renmengwen <mwren1220@163.com>
2026-04-28 00:16:43 +0800

    refactor(About): 移除自定义描述内容以简化页面结构

--- web/src/pages/About/index.jsx
+++ web/src/pages/About/index.jsx
@@ -123,75 +123,8 @@ const About = () => {
   };
 
   const customDescription = (
-    <div style={{ textAlign: 'center' }}>
-      <p>{t('可在设置页面设置关于内容，支持 HTML & Markdown')}</p>
-      {t('New API项目仓库地址：')}
-      <a
-        href='https://github.com/QuantumNous/new-api'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='!text-semi-color-primary'
-      >
-        https://github.com/QuantumNous/new-api
-      </a>
-      <p>
-        <a
-          href='https://github.com/QuantumNous/new-api'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='!text-semi-color-primary'
-        >
-          NewAPI
-        </a>{' '}
-        {t('© {{currentYear}}', { currentYear })}{' '}
-        <a
-          href='https://github.com/QuantumNous'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='!text-semi-color-primary'
-        >
-          QuantumNous
-        </a>{' '}
-        {t('| 基于')}{' '}
-        <a
-          href='https://github.com/songquanpeng/one-api/releases/tag/v0.5.4'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='!text-semi-color-primary'
-        >
-          One API v0.5.4
-        </a>{' '}
-        © 2023{' '}
-        <a
-          href='https://github.com/songquanpeng'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='!text-semi-color-primary'
-        >
-          JustSong
-        </a>
-      </p>
-      <p>
-        {t('本项目根据')}
-        <a
-          href='https://github.com/songquanpeng/one-api/blob/v0.5.4/LICENSE'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='!text-semi-color-primary'
-        >
-          {t('MIT许可证')}
-        </a>
-        {t('授权，需在遵守')}
-        <a
-          href='https://www.gnu.org/licenses/agpl-3.0.html'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='!text-semi-color-primary'
-        >
-          {t('AGPL v3.0协议')}
-        </a>
-        {t('的前提下使用。')}
-      </p>
+    <div >
+
     </div>
   );
 
@@ -205,7 +138,6 @@ const About = () => {
       {shouldRenderStructuredAbout ? (
         <AboutStructuredPage
           config={aboutConfig}
-          protectedAttribution={customDescription}
         />
       ) : aboutLoaded && about === '' ? (
         <div className='flex justify-center items-center h-screen p-8'>
@@ -221,7 +153,6 @@ const About = () => {
             description={t('管理员暂时未设置任何关于内容')}
             style={emptyStyle}
           >
-            {customDescription}
           </Empty>
         </div>
       ) : (
